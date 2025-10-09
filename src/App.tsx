6143b2b3fc6d416d3d17eb3957b85fcd470a20be
krishnapriya U.A <krishnapriyaua@gmail.com>
2025-10-09 22:04:02 +0530

    Created Issuence and Verification page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
-
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Issuance from "./issuance"
+import Verification from "./verification"
 function App() {
   
   return (
     <>
-     <p>Testin</p>
+     <BrowserRouter>
+     <Routes>
+      <Route path="/" element={<Issuance/>}/>
+      <Route path="/verification" element={<Verification/>}/>
+     </Routes>
+     </BrowserRouter>
     </>
   )
 }
---
+++ src/verification.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+import { Button } from "@mui/material"
+
+export default function Verification() {
+  return (
+    <div>
+        <p>Verfication form coming soon</p>
+        <Button><Link to='/'>Issuance</Link></Button>
+    </div>
+      
+  )
+}
