8f6937cc3be2b0445628c9743c0940f6652698a2
krishnapriya U.A <krishnapriyaua@gmail.com>
2025-10-12 19:05:56 +0530

    Fix UI feedback for credential issuance success/error

--- src/pages/issuance.tsx
+++ src/pages/issuance.tsx
@@ -6,27 +6,42 @@ import { useState } from "react";
 import axios,{AxiosError} from "axios";
 import validator from 'validator'
 import { toast } from "react-toastify";
+const Issuance_backend_url = import.meta.env.VITE_ISSUANCE_BACKEND_URL
+import Swal from 'sweetalert2'
 
 export default function Issuance() {
   const [fullname, setFullname] = useState('')
   const [email, setEmail] = useState('')
 
-  const handleSubmit = async() => {
+  const handleSubmit = async(e :any) => {
+    e.preventDefault()
     try {
       if(!validator.isEmail(email)){
           toast.error('Enter valid Email!')
       }
       else{
       if(fullname.trim()!=='' && email.trim()!==''){
-        const response = await axios.post('http://localhost:3000/issue',{fullname,email})
+        console.log('Backend URL:', Issuance_backend_url)
+        const response = await axios.post(`${Issuance_backend_url}/issue`,{fullname,email})
         if(response.data.success){
-          toast.success(response.data.message)
+          // toast.success(response.data.message)
+          Swal.fire({
+            title:'Credential Issued',
+            text:response.data.message,
+            icon:'success'
+          })
           console.log(response.data.Credentials,'Credentials created from back to front')
           setFullname('')
           setEmail('')
         }
         else{
-          toast.info(response.data.message)
+          // toast.info(response.data.message)
+          Swal.fire({
+            title:'Error',
+            text:response.data.message,
+            icon:'error'
+          })
+
         }
       }
       else{
@@ -48,10 +63,10 @@ export default function Issuance() {
         <div className="verify-container">    
             <div className="verify-card">
                <h3 className="verify-title">Issue Credential</h3>
-                <form className="verify-form">
+                <form className="verify-form form">
                    <TextField variant='standard'  fullWidth label='Fullname' value={fullname} onChange={(e)=>setFullname(e.target.value)} required/> <br />
                    <TextField variant="standard"  fullWidth label='Email' value={email} onChange={(e)=>setEmail(e.target.value)} required/> <br />
-                   <Button className="verify-btn" style={{background:'black'}} onClick={handleSubmit} variant="contained">Issue Credential</Button>
+                   <Button type="submit" className="verify-btn" style={{background:'black'}} onClick={handleSubmit} variant="contained">Issue Credential</Button>
                 </form>
             </div>
             <Link className="verify-link" to='/verification'>Verification</Link>
--- src/pages/verification.tsx
+++ src/pages/verification.tsx
@@ -1,30 +1,46 @@
 import { Link } from "react-router-dom"
 import { Button } from "@mui/material"
 import TextField from '@mui/material/TextField';
-import { useState } from "react";
+import { useState  } from "react";
 import axios, {AxiosError} from "axios";
 import validator from 'validator'
 import { toast } from "react-toastify";
 import '../styles/verify.css'
+const VERIFICATION_BACKEND_URL = import.meta.env.VITE_VERIFICATION_BACKEND_URL
+import Swal from 'sweetalert2'
+
 
 export default function Verification() {
   const [fullname, setFullname] = useState('')
   const [email, setEmail] = useState('')
 
-  const handleSubmit = async() => {
+  const handleSubmit = async(e :any) => {
+    e.preventDefault()
     try {
       if(!validator.isEmail(email)){
           toast.error('Enter valid Email!')
       }
       else{
-      const response = await axios.post('http://localhost:5000/verify',{fullname,email})
+      const response = await axios.post(`${VERIFICATION_BACKEND_URL}/verify`,{fullname,email})
       if(response.data.success){
+
         let cred  = response.data.credential
-        toast.success(`${response.data.message} as \n Fullname - ${cred.fullname} \n Email - ${cred.email} \n workerId - ${cred.workerId}`)
+         Swal.fire({
+            title:'Verified',
+            html:`${response.data.message} as <br>  Fullname : ${cred.fullname} <br> Email : ${cred.email} <br> workerId : ${cred.workerId} <br> Issued at : ${cred.issuedAt}`,
+            icon:'success'
+          })
+        // toast.success(`${response.data.message} as \n Fullname - ${cred.fullname} \n Email - ${cred.email} \n workerId - ${cred.workerId}`)
         console.log(response.data.credential)
+        setFullname('')
+        setEmail('')
       }
       else{
-        toast.info(response.data.message)
+        Swal.fire({
+          title:'Error',
+          text:response.data.message,
+          icon:'error'
+        })
       }
     }
     } catch (err) {
@@ -39,11 +55,11 @@ export default function Verification() {
     <div className="verify-container">
       <div className="verify-card">
         <h3 className="verify-title">Verify Credential</h3>
-        <div className="verify-form">
+        <form className="verify-form form">
           <TextField variant='standard'  fullWidth label='Fullname' value={fullname} onChange={(e)=>setFullname(e.target.value)} required/> <br />
           <TextField variant="standard" fullWidth label='Email' value={email} onChange={(e)=>setEmail(e.target.value)} required/> <br />
-          <Button className="verify-btn"  onClick={handleSubmit} variant="contained">Verify Credential</Button>
-        </div>
+          <Button type="submit" onKeyDown={(e) => { e.key === 'Enter' && e.preventDefault(); }} className="verify-btn"  onClick={handleSubmit} variant="contained">Verify Credential</Button>
+        </form>
       </div>
         
       <Link className="verify-link" to='/'>Back to Issuance</Link>
